670ca6cfcfac66561e0b143afa6894067536eefe
clarknes <donald.laplume@mymail.champlain.edu>
2018-07-15 20:47:25 -0400

    Added tile destruction feature. Note STILL HELLA BUGGY
    I have added a grid generation, match detection, tile destruction, tile replacement, and replaced tile distruction. I THINK replaced tile destruction works though I am still uncertain. Once I can switch tiles I that should cover most major features. Then I will convert destruction from being on a button press to after a match has been made.

---
+++ UnityProject/MechaMatch3RPG/Assets/Scripts/GameManager.cs
@@ -0,0 +1,164 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    public Transform grid;
+    public Button updateGridButton;
+
+    public List<Transform> tileList = new List<Transform>();
+
+    public List<Transform> activeGrid = new List<Transform>();
+
+    public List<Transform> tileKillList = new List<Transform>();
+
+    public List<int> xValues = new List<int>();
+
+// Use this for initialization
+void Start () {
+        GenerateGrid();
+
+        for (int i = 0; i < 9; i++)
+        {
+            xValues.Add(new int());
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        updateGridButton.GetComponent<Button>().interactable = UpdateVelocity();
+
+    }
+
+    void GenerateGrid()
+    {
+        for(int rowNum = 0; rowNum < 9; rowNum++)
+        {
+            for(int colNum = 0; colNum < 9; colNum++)
+            {
+                int whichGem = Random.Range(0, 5);
+
+                Transform temp = Instantiate(tileList[whichGem], new Vector3(colNum, rowNum, 0), tileList[whichGem].rotation);
+
+                temp.gameObject.name = colNum + "-" + rowNum;
+
+                temp.parent = grid;
+                temp.GetComponent<Tile_Parent>().gridCords.x = colNum;
+                temp.GetComponent<Tile_Parent>().gridCords.y = rowNum;
+
+                activeGrid.Add(temp);
+            }
+        }
+    }
+
+    public void UpdateGrid()
+    {
+        foreach(Transform fooTrans in activeGrid)
+        {
+            fooTrans.GetComponent<Tile_Parent>().CheckIfShouldBeDestroyed();
+        }
+
+        foreach(Transform fooTrans in tileKillList)
+        {
+            activeGrid.Remove(fooTrans);
+            Destroy(fooTrans.gameObject);
+        }
+
+        tileKillList.Clear();
+        RestockGrid();
+    }
+
+    public bool UpdateVelocity()
+    {
+        foreach (Transform fooTrans in activeGrid)
+        {
+            if(fooTrans.gameObject.GetComponent<Rigidbody>().velocity.y != 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    void RestockGrid()
+    {
+
+        for(int i = 0; i < activeGrid.Count; i++)
+        {
+            switch (Mathf.RoundToInt(activeGrid[i].GetComponent<Tile_Parent>().gridCords.x))
+            {
+                case 0:
+                    activeGrid[i].name = activeGrid[i].GetComponent<Tile_Parent>().gridCords.x + "-" + xValues[0];
+                    activeGrid[i].GetComponent<Tile_Parent>().gridCords = new Vector2(0, xValues[0]);
+                    xValues[0]++;
+                    break;
+                case 1:
+                    activeGrid[i].name = activeGrid[i].GetComponent<Tile_Parent>().gridCords.x + "-" + xValues[1];
+                    activeGrid[i].GetComponent<Tile_Parent>().gridCords = new Vector2(1, xValues[1]);
+                    xValues[1]++;
+                    break;
+                case 2:
+                    activeGrid[i].name = activeGrid[i].GetComponent<Tile_Parent>().gridCords.x + "-" + xValues[2];
+                    activeGrid[i].GetComponent<Tile_Parent>().gridCords = new Vector2(2, xValues[3]);
+                    xValues[2]++;
+                    break;
+                case 3:
+                    activeGrid[i].name = activeGrid[i].GetComponent<Tile_Parent>().gridCords.x + "-" + xValues[3];
+                    activeGrid[i].GetComponent<Tile_Parent>().gridCords = new Vector2(3, xValues[3]);
+                    xValues[3]++;
+                    break;
+                case 4:
+                    activeGrid[i].name = activeGrid[i].GetComponent<Tile_Parent>().gridCords.x + "-" + xValues[4];
+                    activeGrid[i].GetComponent<Tile_Parent>().gridCords = new Vector2(4, xValues[4]);
+                    xValues[4]++;
+                    break;
+                case 5:
+                    activeGrid[i].name = activeGrid[i].GetComponent<Tile_Parent>().gridCords.x + "-" + xValues[5];
+                    activeGrid[i].GetComponent<Tile_Parent>().gridCords = new Vector2(5, xValues[5]);
+                    xValues[5]++;
+                    break;
+                case 6:
+                    activeGrid[i].name = activeGrid[i].GetComponent<Tile_Parent>().gridCords.x + "-" + xValues[6];
+                    activeGrid[i].GetComponent<Tile_Parent>().gridCords = new Vector2(6, xValues[6]);
+                    xValues[6]++;
+                    break;
+                case 7:
+                    activeGrid[i].name = activeGrid[i].GetComponent<Tile_Parent>().gridCords.x + "-" + xValues[7];
+                    activeGrid[i].GetComponent<Tile_Parent>().gridCords = new Vector2(7, xValues[7]);
+                    xValues[7]++;
+                    break;
+                case 8:
+                    activeGrid[i].name = activeGrid[i].GetComponent<Tile_Parent>().gridCords.x + "-" + xValues[8];
+                    activeGrid[i].GetComponent<Tile_Parent>().gridCords = new Vector2(8, xValues[8]);
+                    xValues[8]++;
+                    break;
+            }
+        }
+
+        for(int i = 0; i < xValues.Count; i++)
+        {
+            for(int j = 8; j >= xValues[i]; j--)
+            {
+                int whichGem = Random.Range(0, 2);
+
+                Transform temp = Instantiate(tileList[whichGem], new Vector3(i, j, 0), tileList[whichGem].rotation);
+
+                temp.gameObject.name = i + "-" + j;
+
+                temp.parent = grid;
+                temp.GetComponent<Tile_Parent>().gridCords.x = i;
+                temp.GetComponent<Tile_Parent>().gridCords.y = j;
+
+                activeGrid.Add(temp);
+            }
+        }
+
+        for(int i = 0; i < xValues.Count; i++)
+        {
+            xValues[i] = 0;
+        }
+    }
+}
--- UnityProject/MechaMatch3RPG/Assets/Scripts/Tile_Parent.cs
+++ UnityProject/MechaMatch3RPG/Assets/Scripts/Tile_Parent.cs
@@ -4,13 +4,124 @@ using UnityEngine;
 
 public class Tile_Parent : MonoBehaviour {
 
+    //This Enum will be used to define ALL tile color systems. All children tiles will share this enum
+    public enum TileColor
+    {
+        Blue,
+        Green,
+        Purple,
+        Red,
+        Yellow
+    }
+
+    //Neccessary Variables to cover whether this tile should be destroyed
+    public GameManager gm;
+    public TileColor m_tileColor;
+    public bool shouldBeDestroyed;
+    public Vector2 gridCords;
+
+    //DebugTools
+    public bool shouldOutputDebug;
+
 	// Use this for initialization
 	void Start () {
-		
+        gm = FindObjectOfType<GameManager>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void CheckIfShouldBeDestroyed()
+    {
+        if (shouldOutputDebug)
+        {
+            Debug.Log("Test point 1" + shouldBeDestroyed);
+        }
+
+        if(CheckHorizontal() || CheckVertical())
+        {
+            if (shouldOutputDebug)
+            {
+                Debug.Log("Test point CheckH/CheckV" + shouldBeDestroyed);
+            }
+
+            shouldBeDestroyed = true;
+            gm.tileKillList.Add(transform);
+        }
+
+        if (shouldOutputDebug)
+        {
+            Debug.Log("Test point final" + shouldBeDestroyed);
+        }
+    }
+
+    public bool CheckHorizontal()
+    {
+        int xMinus, xPlus;
+        xMinus = Mathf.RoundToInt(gridCords.x) - 1;
+        xPlus = Mathf.RoundToInt(gridCords.x) + 1;
+
+        Transform left = transform.parent.gameObject.transform.Find(xMinus + "-" + gridCords.y);
+        Transform right = transform.parent.gameObject.transform.Find(xPlus + "-" + gridCords.y);
+
+        if (shouldOutputDebug)
+        {
+            Debug.Log("Test point CheckHorizontal" + xMinus + " " + xPlus );
+        }
+
+        if (left && right)
+        {
+            if(left.GetComponent<Tile_Parent>() && right.GetComponent<Tile_Parent>())
+            {
+                if(left.GetComponent<Tile_Parent>().m_tileColor == m_tileColor && right.GetComponent<Tile_Parent>().m_tileColor == m_tileColor)
+                {
+                    left.GetComponent<Tile_Parent>().SetDestroyOverRide();
+                    right.GetComponent<Tile_Parent>().SetDestroyOverRide();
+
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public bool CheckVertical()
+    {
+        int yMinus, yPlus;
+        yMinus = Mathf.RoundToInt(gridCords.y) - 1;
+        yPlus = Mathf.RoundToInt(gridCords.y) + 1;
+
+        Transform down = transform.parent.gameObject.transform.Find(gridCords.x + "-" + yMinus);
+        Transform up = transform.parent.gameObject.transform.Find(gridCords.x + "-" + yPlus);
+
+        if (shouldOutputDebug)
+        {
+            Debug.Log("Test point CheckVert" + down.name + " " + up.name);
+        }
+
+        if (down && up)
+        {
+            if (down.GetComponent<Tile_Parent>() && up.GetComponent<Tile_Parent>())
+            {
+                if (down.GetComponent<Tile_Parent>().m_tileColor == m_tileColor && up.GetComponent<Tile_Parent>().m_tileColor == m_tileColor)
+                {
+                    down.GetComponent<Tile_Parent>().SetDestroyOverRide();
+                    up.GetComponent<Tile_Parent>().SetDestroyOverRide();
+
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public void SetDestroyOverRide()
+    {
+        shouldBeDestroyed = true;
+        gm.tileKillList.Add(transform);
+    }
 }
